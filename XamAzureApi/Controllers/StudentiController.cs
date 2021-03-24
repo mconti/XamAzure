a2323421b8c40b990c12063a17918721c56e5d86
mconti <posta@maurizioconti.com>
2021-03-24 17:41:53 +0100

    Modificato material web

--- XamAzureApi/Controllers/StudentiController.cs
+++ XamAzureApi/Controllers/StudentiController.cs
@@ -18,6 +18,7 @@ namespace XamAzureApi.Controllers
             List<Studente> studenti = new List<Studente>();
             studenti.Add(new Studente { Nome = "Maurizio", Cognome = "Conti", Avatar = "maurizio.conti.png", EMail = "maurizio.conti@ittsrimini.edu.it" });
             studenti.Add(new Studente { Nome = "Fabio", Cognome = "Corbelli", Avatar = "fabio.corbelli.png", EMail = "fabio.corbelli@ittsrimini.edu.it" });
+            studenti.Add(new Studente { Nome = "Riccardo", Cognome = "Bianchi", Avatar = "riccardo.bianchi.png", EMail = "riccardo.bianchi@ittsrimini.edu.it" });
 
             return studenti;
         }
